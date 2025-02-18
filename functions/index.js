91b1d3683774b545a8c69d30ff1fd783fb4a0a17
最好喝的小米粥 <139665582+cunyx-xiaomizhou@users.noreply.github.com>
2025-02-18 18:22:53 +0800

    🍲 导出模块

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import { info } from './info.js';
 import { load } from './load.js';
 import { config } from './../config/config.js';
-const pp = `${process.cwd()}/plugins/GuGuZhou-Plugin`
+const pp = `${process.cwd()}/plugins/${await info()).name}`
 let ggz = {
   info: info,
   load: load,
