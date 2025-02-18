6232be49c45230f1475630eae8064ebb61edb05a
最好喝的小米粥 <139665582+cunyx-xiaomizhou@users.noreply.github.com>
2025-02-18 17:01:26 +0800

    更新 apps

---
+++ apps/t.js
@@ -0,0 +1,15 @@
+import ggz from '#ggz';
+export class ggz_plugin_t extends plugin {
+  constructor () {
+    super({
+      name:"ggz",
+      dsc:"ggz",
+      event:"message",
+      priority:1,/*优先级*/
+      rule:[
+        {reg:"^#?ggzt",fnc:"ggzt",}
+      ]
+    });
+  }
+  async ggzt() {e.reply(ggz.info.name)}
+}
