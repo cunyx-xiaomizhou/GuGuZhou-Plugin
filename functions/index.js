d28a1c02968f4167d92015ac12cba8a075743a80
最好喝的小米粥 <139665582+cunyx-xiaomizhou@users.noreply.github.com>
2025-02-18 16:20:34 +0800

    更新 index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import { info } from './info.js';
 import { load } from './load.js';
 import { config } from './../config/config.js';
-const pp = `${process.cwd()}/plugins/${info().name}`
+const pp = `${process.cwd()}/plugins/${info.name}`
 let ggz = {
   info: info,
   load: load,
