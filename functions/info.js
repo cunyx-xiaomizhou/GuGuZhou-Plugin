b074880ca084a6a8d1d16ae8e9361ebc33f5479d
最好喝的小米粥 <139665582+cunyx-xiaomizhou@users.noreply.github.com>
2025-02-18 16:45:45 +0800

    更新 info.js

--- functions/info.js
+++ functions/info.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 async function info() {
   let pluginPath = `${process.cwd()}/plugins/GuGuZhou-plugin/`;
-  let pluginData = JSON.parse(await fs.readFileSync(`${pluginPath}package.json`, 'utf8'));
+  let pluginData = await JSON.parse(await fs.readFileSync(`${pluginPath}package.json`, 'utf8'));
   return pluginData;
 }
 
