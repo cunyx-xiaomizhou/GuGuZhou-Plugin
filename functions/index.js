fe9f9f7131eb456af366c86c010e306d62f05864
最好喝的小米粥 <139665582+cunyx-xiaomizhou@users.noreply.github.com>
2025-02-18 15:01:16 +0800

    🍓 导出模块

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 import { info } from './info.js';
 import { load } from './load.js';
 import { config } from './../config/config.js';
+const pp = `${process.cwd()}/plugins/${info().name}`
 let ggz = {
   info: info,
   load: load,
-  config: config
+  config: config,
+  path: pp
 };
 export default ggz;
