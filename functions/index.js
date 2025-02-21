2d8c55e7b312d09260ed78ef3cac59aa5fd92556
最好喝的小米粥 <admin@cunyx.cn>
2025-02-21 07:54:34 +0000

    ☺️ 换一个归宿
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,31 @@ import { load } from './load.js';
 import pkg from './pkg/index.js';
 import { config } from './../config/config.js';
 const pp = `${process.cwd()}/plugins/${(await info()).name}`
+
+async function game(e) {
+  const msg = e.msg.toLowerCase();
+  const regex = /^(ggz|咕咕粥)下载((原神|ys|gs|genshin)|(崩坏：星穹铁道|崩铁|星铁|sr)|(绝区零|zzz)|(鸣潮|ww|waves?))(图包)?$/gi;
+  const match = regex.exec(msg);
+  if (match) {
+    if (match[3]) {
+      return "YS";
+    } else if (match[4]) {
+      return "SR";
+    } else if (match[5]) {
+      return "ZZZ";
+    } else if (match[6]) {
+      return "WW";
+    }
+  }
+  return false;
+}
+
 let ggz = {
   info: info,
   load: load,
   config: config,
   path: pp,
-  pkg: pkg
+  pkg: pkg,
+  game: game
 };
 export default ggz;
