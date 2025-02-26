45afb15a3233a87cbdb7d9fbf7ed49a62240a079
最好喝的小米粥 <admin@cunyx.cn>
2025-02-26 02:08:08 +0000

    🍂 简单写写
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/state.js
+++ apps/state.js
@@ -1,3 +1,4 @@
+import ggz from '#ggz';
 export class ggz_state extends plugin {
   constructor () {
     super({
@@ -14,6 +15,37 @@ export class ggz_state extends plugin {
     });
   }
   async state(e) {
-    //
+    let gz_view = false;
+    var source = await ggz.config('source', 'source');
+    try {
+        const url = `${source}/raw/main/package.json`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`网络请求失败: ${response.statusText}`);
+        }
+        const viewInfo = await response.json();
+        if (viewInfo && viewInfo.version) {
+            gz_view = viewInfo.version;
+        } else {
+            throw new Error('网络 package.json 格式错误');
+        }
+    } catch (error) {
+        logger.error('获取网络版本失败:', error.message);
+        gz_view = false;
+    }
+    let state_info = {
+      uin: Bot.uin || 2996849867,
+      nick: Bot.nickname || '云崽机器人',
+      ggz_version: (await ggz.info()).version,
+      ggz_version_view: gz_view,
+      ggz_ys_version: gs.local || '未安装',
+      ggz_ys_version_view: gs.view,
+      ggz_sr_version: sr.local || '未安装',
+      ggz_sr_version_view: sr.view,
+      ggz_zzz_version: zzz.local || '未安装',
+      ggz_zzz_version_view: zzz.view,
+      ggz_ww_version: ww.local || '未安装',
+      ggz_ww_version_view: ww.view
+    };
   }
 }
