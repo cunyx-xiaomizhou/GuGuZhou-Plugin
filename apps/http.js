4f646a7e76da71279131525f2235b5376d700a0d
Admilk <13205155+adrae@user.noreply.gitee.com>
2025-03-02 00:30:53 +0800

    代码和我总有一个能跑

--- apps/http.js
+++ apps/http.js
@@ -12,12 +12,18 @@ Router.use("/:keys",async (req,res,next)=>handler(req,res,next))
   const app = express();
   app.use('/GuGuZhou', express.static(resourcePath));
   app.use('/GuGuZhou/:keys', handler);
+  app.use("/GuGuZhou/font/:keys",handler)
   Bot.express = app
   Bot.server = Bot.express.listen(2536, () => {
     logger.info('[GuGuZhou] server started on port 2536');
   })
 }
 async function handler(req, res, _next) {
+  if (req.params.keys.includes("ttf")||req.params.keys.includes("woff")||req.params.keys.includes("woff2")) {
+    const filePath = path.join(resourcePath, 'font', req.params.keys);
+    res.sendFile(filePath);
+    return
+  }
   const filePath = path.join(resourcePath, 'img', req.params.keys+".png");
   res.sendFile(filePath);
 }
