22b5f76872ee084f7749f758043fa7040ba5883b
Admilk <admin@admilk.top>
2025-03-01 10:18:46 +0000

    Revert "update index.js."
    This reverts commit 288ba63176a40def96e04a2fd4cb857fe002af7f.

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
+import fs from 'node:fs';
 import ggz from '#ggz';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const defDir = path.join(ggz.path, 'config/default');
-const configDir = path.join(ggz.path, 'config/config');
-await ggz.load(configDir, defDir);
-const appsDir = path.join(__dirname, `./apps`);
-const apps = {};
-  const updatePath = path.join(appsDir, 'update.js');
-  try {
-    const updateModule = await import(updatePath);
-    apps.update = updateModule[Object.keys(updateModule)[0]];
-  } catch (error) {
-    logger.error(error);
+const defDir = ggz.path + '/config/default';
+const configDir = ggz.path + '/config/config';
+ggz.load(configDir,defDir);
+const files = fs.readdirSync(`./plugins/${(await ggz.info()).name}/apps`).filter(file => file.endsWith('.js'));
+let ret = [];
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`));
+});
+ret = await Promise.allSettled(ret);
+let apps = {};
+for (let i in files) {
+  let name = files[i].replace('.js', '');
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`);
+    logger.error(ret[i].reason);
+    continue;
   }
-
-  // 并行加载其他模块
-  const files = (await fs.readdir(appsDir)).filter(file => 
-    file.endsWith('.js') && file !== 'update.js'
-  );
-
-  const results = await Promise.allSettled(
-    files.map(file => import(path.join(appsDir, file)))
-  );
-
-  // 处理加载结果
-  results.forEach((result, index) => {
-    const name = path.basename(files[index], '.js');
-    if (result.status === 'fulfilled') {
-      apps[name] = result.value[Object.keys(result.value)[0]];
-    } else {
-      logger.warn(`[咕咕粥插件] 可选模块 [${name}] 加载跳过`);
-      logger.debug(result.reason);
-    }
-  });
-
-  export { apps };
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]];
+}
+export { apps };
